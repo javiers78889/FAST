2fa99d609e0e4617aa762978d660b6430c1c70f3
javiers78889 <javiers78889@gmail.com>
2024-11-22 03:06:23 -0500

    al fin

--- src/widgets/HeroeItems/HeroePhone.jsx
+++ src/widgets/HeroeItems/HeroePhone.jsx
@@ -57,10 +57,11 @@ export const HeroePhone = ({ mobileMenuOpen, cierraModal, navigation }) => {
                         <div className="py-6">
                             <a
                                 href="#"
+                                onClick={cierraModal}
                                 className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                             >
                                 Log in
-                                onClick={cierraModal}
+                               
                             </a>
                         </div>
                     </div>
